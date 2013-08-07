a55e0969fe47fc5a27ee379b10c2a57836bac144
venkatror <venkat0535@gmail.com>
2013-08-07 14:46:45 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rickrockstar::Application.config.secret_token = '56d619d7b7b2aadcda9cb35dba9a37dc7691dffade66f263ec0f3fac2374db324a503c10a4296790984d0513061f2ccc8bdd6100cea3233ebcf3f978f39352bf'
---
+++ rickrockstar/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rickrockstar::Application.config.secret_token = '1b79080292af9eb910d9d1c7ed3139cd9854fdbd9f24a8d064aa55796b4b50a08dc64daac35f16d9236edab3902f9da3a630a796b239fd6ae27c84d7dd21b156'
