8c80974a2ac9805a9b87a7d209c4c9dd255fd830
venkatror <venkat0535@gmail.com>
2013-08-08 11:39:48 +0530

    hi

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rickrockstar::Application.config.secret_token = '56d619d7b7b2aadcda9cb35dba9a37dc7691dffade66f263ec0f3fac2374db324a503c10a4296790984d0513061f2ccc8bdd6100cea3233ebcf3f978f39352bf'
+Rickrockstar::Application.config.secret_token = '1b79080292af9eb910d9d1c7ed3139cd9854fdbd9f24a8d064aa55796b4b50a08dc64daac35f16d9236edab3902f9da3a630a796b239fd6ae27c84d7dd21b156'
--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Rickrockstar::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'pages#home'
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
